7a748795fb560dc3a02919d8a134c613c6074ecb
Fran <franorquera88@gmail.com>
2021-04-08 16:41:38 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// Import Pages
+import AboutUs from "./pages/AboutUs";
+
+function App() {
+  return (
+    <div className="App">
+      <AboutUs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,14 @@
+// Import Components
+import AboutSection from "../components/AboutSectionComponent";
+import ServicesSection from "../components/ServicesSectionComponent";
+
+const AboutUs = () => {
+    return (
+        <div className="MainPage">
+            <AboutSection />
+            <ServicesSection />
+        </div>
+    )
+}
+
+export default AboutUs;
